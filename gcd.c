056febf54592d97c565d703f3c56fda86c89cf5c
SARTHAK ANDHARE <sarthakandhare000@gmail.com>
2025-08-09 23:54:07 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,21 @@
+void main()
+{
+    int x,y,i;
+    printf("Enter a 2  numbers:");
+    scanf("%d %d",&x,&y);
+    for(i=x<y?x:y;i>0;i--)
+    {
+        if(x%i==0 && y%i==0)
+        {
+            printf("GCD is %d",i);
+            break;
+        }
+
+    }
+
+
+
+
+
+    getch();
+}
