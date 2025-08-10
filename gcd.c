62f43eb74eb2341db683358456da80a268f25691
SARTHAK ANDHARE <sarthakandhare000@gmail.com>
2025-08-10 10:59:30 +0530

    Update gcd.c

--- gcd.c
+++ gcd.c
@@ -1,21 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+
 void main()
 {
-    int x,y,i;
-    printf("Enter a 2  numbers:");
-    scanf("%d %d",&x,&y);
-    for(i=x<y?x:y;i>0;i--)
+    int x, y, i, start;
+
+    printf("Enter 2 numbers: ");
+    scanf("%d%d", &x, &y);
+
+    // Find smaller number and store in start
+    if (x < y)
+        start = x;
+    else
+        start = y;
+
+    // Initialize i in the for loop
+    for (i = start; i > 0; i--)
     {
-        if(x%i==0 && y%i==0)
+        if (x % i == 0 && y % i == 0)
         {
-            printf("GCD is %d",i);
+            printf("GCD is %d", i);
             break;
         }
-
     }
-
-
-
-
 
     getch();
 }
